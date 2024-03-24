e1a239f33ce9e229c0f238c7b48546f18c82142a
Kalli <kallirant@gmail.com>
2024-03-25 00:58:32 +0200

    fix lines

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -7,7 +7,7 @@ import time
 from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management.base import BaseCommand
-from django.db.utils import OperationalError 
+from django.db.utils import OperationalError
 
 class Command(BaseCommand):
     """Djangp command to wait for db."""
@@ -27,4 +27,3 @@ class Command(BaseCommand):
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('DB AVAILABLE!'))
-
--- app/core/tests/tests_commands.py
+++ app/core/tests/tests_commands.py
@@ -13,9 +13,10 @@ from django.test import SimpleTestCase
 # mock behavior: path + command.check which is provided by base command
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTest(SimpleTestCase):
-    
-    # patched_check arg -> catches the returned value (response) from check method
-    def test_wiat_for_db_ready(self, patched_check):
+
+    # patched_check arg ->
+    # catches the returned value (response) from check method
+    def test_wait_for_db_ready(self, patched_check):
         """Test waiting for db  if db is ready."""
 
         patched_check.return_value = True
@@ -27,7 +28,7 @@ class CommandTest(SimpleTestCase):
         """Test waiting for db when getting Operational error"""
         patched_check.side_effect = [Psycoppg2Error] * 2 + \
             [OperationalError] * 3 + [True]
-        
+
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
