ddc2c80d0396f080d48a7297c700af2228ba13ab
Kalli <kallirant@gmail.com>
2024-03-24 23:36:44 +0200

    reemove lines

--- app/app/tests.py
+++ app/app/tests.py
@@ -3,20 +3,16 @@ Sample tests
 """
 
 from django.test import SimpleTestCase
-
 from app import calc
 
 class CaclTests(SimpleTestCase):
-
     def test_add_numbers(self):
         """
         Adding numbers together
         """
         res = calc.add(5, 6)
-
         self.assertEqual(res, 11)
 
     def test_subtract_number(self):
         res = calc.subtract(10, 15)
-
         self.assertEqual(res, 5)
