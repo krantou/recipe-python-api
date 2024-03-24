9ce7e3930f5c696bc6e6c97d8d6b17aa21cae8e5
Kalli <kallirant@gmail.com>
2024-03-25 00:24:12 +0200

    fix import in unittest

--- app/core/tests/tests_commands.py
+++ app/core/tests/tests_commands.py
@@ -2,7 +2,7 @@
 Test custom management commands.
 """
 
-from unittest import patch
+from unittest.mock import patch
 
 from psycopg2 import OperationalError as Psycoppg2Error
 from django.core.management import call_command
