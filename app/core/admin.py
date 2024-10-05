f4afb21d2f77d52dddd1caf25325c704f8f8db02
Kalli <kallirant@gmail.com>
2024-10-05 15:58:11 +0300

    build user API

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin # noqa
 
-# Register your models here.
+'''
+Django admin customization.
+We need to enable django admin for our user.
+'''
+
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUseradmin
+from django.utils.translation import gettext_lazy as _
+
+from core import models
+
+class UserAdmin(BaseUseradmin):
+    '''Define the admin pages for users'''
+
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Permission'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            )
+        }),
+    )
+
+
+# We need to register our user model.
+# we need to specify user admin so as to have the
+# customization we added above
+admin.site.register(models.User, UserAdmin)
---
+++ app/core/tests/tests_model.py
@@ -0,0 +1,49 @@
+"""
+Tests for models
+"""
+
+from django.test import TestCase
+# get_user_model helper fun, provided by django, ref to custom user model
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_succssful(self):
+        """Test creating a  user with an email successfully."""
+
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users"""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['test2@Example.com', 'test2@example.com'],
+            ['TEST3@Example.com', 'TEST3@example.com'],
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """New user without email raises error."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', '123')
+
+    def test_create_superuser(self):
+        """Test creating superuser."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
